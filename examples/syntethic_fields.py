e600626b6b9c3274762b1e5b7e642ad8d38d98c0
Christophe Vauclair <cfvauclair@gmail.com>
2021-11-25 09:41:37 -0500

    Update example

--- examples/syntethic_fields.py
+++ examples/syntethic_fields.py
@@ -4,19 +4,21 @@ from dash import html
 from datetime import datetime
 
 from subgrounds.components import LinePlot
-from subgrounds.query import SyntheticField, OrderDirection
-from subgrounds.subgraph import Subgraph
+from subgrounds.subgraph import Subgraph, SyntheticField
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-uniswapV2 = Subgraph("uniswapV2", "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2")
+uniswapV2 = Subgraph.of_url("https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2")
 
 # Define the query object
+Query = uniswapV2.Query
+
 Swap = uniswapV2.Swap
 
-Swap.price = abs(Swap.amount0_in - Swap.amount0_out) / abs(Swap.amount1_in - Swap.amount1_out)
+Swap.price = abs(Swap.amount0In - Swap.amount0Out) / abs(Swap.amount1In - Swap.amount1Out)
 
 Swap.datetime = SyntheticField(
+  uniswapV2,
   lambda timestamp: str(datetime.fromtimestamp(timestamp)),
   Swap.timestamp
 )
@@ -30,9 +32,9 @@ app.layout = html.Div(
     html.Div(id='step-display'),
     html.Div([
       LinePlot(
-        Swap, 
-        order_by=Swap.timestamp,
-        order_direction=OrderDirection.DESC,
+        Query.swaps, 
+        orderBy=Swap.timestamp,
+        orderDirection="desc",
         first=100,
         where=[
           Swap.pair == "0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc"
