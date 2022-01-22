ec01bd34ebec5ba621546ecd6f39951731a325bf
cvauclair <cfvauclair@gmail.com>
2022-01-22 11:50:03 -0500

    Update test_examples.sh
    Increase dash grace period again

--- tests/test_examples.sh
+++ tests/test_examples.sh
@@ -6,7 +6,7 @@ for f in examples/*.py; do
   cp $f .
   pipenv run python $(basename $f) &
   DASH_PID=$!
-  sleep 10
+  sleep 20
   RESP_CODE=$(curl --head --location --write-out %{http_code} --silent --output /dev/null http://127.0.0.1:8050/)
   # echo $RESP_CODE
   # kill -9 $DASH_PID
