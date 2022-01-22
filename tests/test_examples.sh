18cbb03a02a863f4038f458d00fd60fb2d68a814
Christophe Vauclair <cfvauclair@gmail.com>
2022-01-22 11:55:26 -0500

    Cleanup smoketest script

--- tests/test_examples.sh
+++ tests/test_examples.sh
@@ -5,11 +5,8 @@ FAILING=0
 for f in examples/*.py; do
   cp $f .
   pipenv run python $(basename $f) &
-  DASH_PID=$!
   sleep 20
   RESP_CODE=$(curl --head --location --write-out %{http_code} --silent --output /dev/null http://127.0.0.1:8050/)
-  # echo $RESP_CODE
-  # kill -9 $DASH_PID
   kill `lsof -w -n -i tcp:8050 | awk '$2!="PID" {print $2;}'`
   rm $(basename $f)
   if [ "$RESP_CODE" != "200" ];
