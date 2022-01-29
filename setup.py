ca2f20202e599b81f0e0c62ef0fdfd751c66b6e0
Christophe Vauclair <cfvauclair@gmail.com>
2022-01-29 16:54:21 -0500

    Bump subgrounds version

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+  long_description = fh.read()
 
 setuptools.setup(
-    name="subgrounds",
-    version="0.0.3",
-    author="Protean Labs",
-    author_email="info@protean.so",
-    description="A framework for integrating The Graph data with dash components.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Protean-Labs/subgrounds",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License"
-    ],
-    packages=setuptools.find_packages(exclude=["tests", "examples"]),
-    install_requires=['dash', 'requests', 'pipe', 'pandas'],
-    python_requires=">=3.10",
+  name="subgrounds",
+  version="0.0.4",
+  author="Protean Labs",
+  author_email="info@protean.so",
+  description="A framework for integrating The Graph data with dash components.",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/Protean-Labs/subgrounds",
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: Apache Software License"
+  ],
+  packages=setuptools.find_packages(exclude=["tests", "examples"]),
+  install_requires=['dash', 'requests', 'pipe', 'pandas'],
+  python_requires=">=3.10",
 )
