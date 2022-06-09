30e4ec1a553f4f8ea9ab1562161bbdfa1dc5e07f
Christophe Vauclair <cfvauclair@gmail.com>
2022-06-09 13:54:09 -0400

    test: update `test_utils` to use pytest

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,146 +2,157 @@ import operator
 import unittest
 from dataclasses import dataclass
 
+import pytest
+
 from subgrounds.utils import extract_data, flatten_dict, intersection, rel_complement, union
 
 
-class TestSetUtils(unittest.TestCase):
-  def test_rel_complement_1(self):
-    l1 = [1, 2, 3]
-    l2 = [3, 4, 5]
-    self.assertEqual(rel_complement(l1, l2), [1, 2])
+def test_rel_complement_1():
+  l1 = [1, 2, 3]
+  l2 = [3, 4, 5]
+  assert rel_complement(l1, l2) == [1, 2]
 
-  def test_rel_complement_2(self):
-    l1 = [3, 4]
-    l2 = [3, 4, 5]
-    self.assertEqual(rel_complement(l1, l2), [])
 
-  def test_rel_complement_3(self):
-    @dataclass
-    class X:
-      id: int
-      txt: str
+def test_rel_complement_2():
+  l1 = [3, 4]
+  l2 = [3, 4, 5]
+  assert rel_complement(l1, l2) == []
 
-    l1 = [X(1, 'hello'), X(2, 'bob'), X(6, 'world!')]
-    l2 = [X(5, 'abcd'), X(6, 'message')]
 
-    self.assertEqual(rel_complement(l1, l2, key=lambda x: x.id), [X(1, 'hello'), X(2, 'bob')])
+def test_rel_complement_3():
+  @dataclass
+  class X:
+    id: int
+    txt: str
 
-  def test_intersection_1(self):
-    l1 = [1, 2, 3]
-    l2 = [3, 4, 5]
-    self.assertEqual(intersection(l1, l2), [3])
+  l1 = [X(1, 'hello'), X(2, 'bob'), X(6, 'world!')]
+  l2 = [X(5, 'abcd'), X(6, 'message')]
 
-  def test_intersection_2(self):
-    l1 = [1, 2, 3]
-    l2 = [3, 4, 5]
-    self.assertEqual(intersection(l1, l2, combine=operator.add), [6])
+  assert rel_complement(l1, l2, key=lambda x: x.id) == [X(1, 'hello'), X(2, 'bob')]
 
-  def test_intersection_3(self):
-    l1 = [1, 2]
-    l2 = [3, 4, 5]
-    self.assertEqual(intersection(l1, l2), [])
 
-  def test_intersection_4(self):
-    @dataclass
-    class X:
-      id: int
-      txt: str
+def test_intersection_1():
+  l1 = [1, 2, 3]
+  l2 = [3, 4, 5]
+  assert intersection(l1, l2) == [3]
 
-    l1 = [X(1, 'hello'), X(2, 'bob'), X(6, 'world!')]
-    l2 = [X(5, 'abcd'), X(6, 'message')]
 
-    self.assertEqual(intersection(l1, l2, key=lambda x: x.id, combine=lambda x, y: X(x.id, f'{x.txt} {y.txt}')), [X(6, 'world! message')])
+def test_intersection_2():
+  l1 = [1, 2, 3]
+  l2 = [3, 4, 5]
+  assert intersection(l1, l2, combine=operator.add) == [6]
 
-  def test_union_1(self):
-    l1 = [1, 2, 3]
-    l2 = [3, 4, 5]
-    self.assertEqual(union(l1, l2), [1, 2, 3, 4, 5])
 
-  def test_union_2(self):
-    l1 = [1, 2, 3]
-    l2 = [3, 4, 5]
-    self.assertEqual(union(l1, l2, combine=operator.add), [1, 2, 6, 4, 5])
+def test_intersection_3():
+  l1 = [1, 2]
+  l2 = [3, 4, 5]
+  assert intersection(l1, l2) == []
 
-  def test_union_3(self):
-    @dataclass
-    class X:
-      id: int
-      txt: str
 
-    l1 = [X(1, 'hello'), X(2, 'bob'), X(6, 'world!')]
-    l2 = [X(5, 'abcd'), X(6, 'message')]
+def test_intersection_4():
+  @dataclass
+  class X:
+    id: int
+    txt: str
 
-    expected = [
-      X(1, 'hello'),
-      X(2, 'bob'),
-      X(6, 'world! message'),
-      X(5, 'abcd')
+  l1 = [X(1, 'hello'), X(2, 'bob'), X(6, 'world!')]
+  l2 = [X(5, 'abcd'), X(6, 'message')]
+
+  assert intersection(l1, l2, key=lambda x: x.id, combine=lambda x, y: X(x.id, f'{x.txt} {y.txt}')) == [X(6, 'world! message')]
+
+
+def test_union_1():
+  l1 = [1, 2, 3]
+  l2 = [3, 4, 5]
+  assert union(l1, l2) == [1, 2, 3, 4, 5]
+
+
+def test_union_2():
+  l1 = [1, 2, 3]
+  l2 = [3, 4, 5]
+  assert union(l1, l2, combine=operator.add) == [1, 2, 6, 4, 5]
+
+
+def test_union_3():
+  @dataclass
+  class X:
+    id: int
+    txt: str
+
+  l1 = [X(1, 'hello'), X(2, 'bob'), X(6, 'world!')]
+  l2 = [X(5, 'abcd'), X(6, 'message')]
+
+  expected = [
+    X(1, 'hello'),
+    X(2, 'bob'),
+    X(6, 'world! message'),
+    X(5, 'abcd')
+  ]
+
+  assert union(l1, l2, key=lambda x: x.id, combine=lambda x, y: X(x.id, f'{x.txt} {y.txt}')) == expected
+
+
+def test_extract_data_1():
+  expected = [1, 2, 3]
+
+  data = {
+    'a': [
+      {'b': {'c': 1}},
+      {'b': {'c': 2}},
+      {'b': {'c': 3}},
     ]
+  }
 
-    self.assertEqual(union(l1, l2, key=lambda x: x.id, combine=lambda x, y: X(x.id, f'{x.txt} {y.txt}')), expected)
+  path = ['a', 'b', 'c']
+
+  assert extract_data(path, data) == expected
 
 
-class TestExtractData(unittest.TestCase):
-  def test_extract_data_1(self):
-    expected = [1, 2, 3]
+def test_extract_data_2():
+  expected = []
 
-    data = {
-      'a': [
-        {'b': {'c': 1}},
-        {'b': {'c': 2}},
-        {'b': {'c': 3}},
-      ]
-    }
+  data = {
+    'a': []
+  }
 
-    path = ['a', 'b', 'c']
+  path = ['a', 'b', 'c']
 
-    self.assertEqual(extract_data(path, data), expected)
+  assert extract_data(path, data) == expected
 
-  def test_extract_data_2(self):
-    expected = []
 
-    data = {
-      'a': []
-    }
+def test_extract_data_3():
+  expected = []
 
-    path = ['a', 'b', 'c']
+  data = {
+    'a': []
+  }
 
-    self.assertEqual(extract_data(path, data), expected)
+  path = ['a']
 
-  def test_extract_data_3(self):
-    expected = []
+  assert extract_data(path, data) == expected
 
-    data = {
-      'a': []
-    }
 
-    path = ['a']
+def tests_flatten_dict_1():
+  expected = {
+    'x': 1,
+    'a_b_c': 10,
+    'd_e': 'hello',
+    'd_f': 'world',
+    'foo': True
+  }
 
-    self.assertEqual(extract_data(path, data), expected)
+  data = {
+    'x': 1,
+    'a': {
+      'b': {
+        'c': 10
+      }
+    },
+    'd': {
+      'e': 'hello',
+      'f': 'world'
+    },
+    'foo': True
+  }
 
-class TestFlattenDict(unittest.TestCase):
-  def tests_flatten_dict_1(self):
-    expected = {
-      'x': 1,
-      'a_b_c': 10,
-      'd_e': 'hello',
-      'd_f': 'world',
-      'foo': True
-    }
-
-    data = {
-      'x': 1,
-      'a': {
-        'b': {
-          'c': 10
-        }
-      },
-      'd': {
-        'e': 'hello',
-        'f': 'world'
-      },
-      'foo': True
-    }
-
-    self.assertEqual(flatten_dict(data), expected)
+  assert flatten_dict(data) == expected
